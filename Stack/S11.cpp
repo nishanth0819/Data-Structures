912f03f63fe3b9e942c6495461fa67558c7189c1
nishanth0819 <87228964+nishanth0819@users.noreply.github.com>
2022-02-12 11:34:15 +0530

    Implement Stack Using Queue

---
+++ Stack/S11.cpp
@@ -0,0 +1,86 @@
+// Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+
+// Implement the MyStack class:
+
+// void push(int x) Pushes element x to the top of the stack.
+// int pop() Removes the element on the top of the stack and returns it.
+// int top() Returns the element on the top of the stack.
+// boolean empty() Returns true if the stack is empty, false otherwise.
+// Notes:
+
+// You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
+// Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations.
+ 
+
+// Example 1:
+
+// Input
+// ["MyStack", "push", "push", "top", "pop", "empty"]
+// [[], [1], [2], [], [], []]
+// Output
+// [null, null, null, 2, 2, false]
+
+// Explanation
+// MyStack myStack = new MyStack();
+// myStack.push(1);
+// myStack.push(2);
+// myStack.top(); // return 2
+// myStack.pop(); // return 2
+// myStack.empty(); // return False
+ 
+
+// Constraints:
+
+// 1 <= x <= 9
+// At most 100 calls will be made to push, pop, top, and empty.
+// All the calls to pop and top are valid.
+
+
+#CODE:
+
+class MyStack {
+public:
+    queue<int> q1,q2;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        if(q1.empty()==true)
+            q1.push(x);
+        else
+        {
+            while(q1.empty()==false)
+            {
+                q2.push(q1.front());
+                q1.pop();
+            }
+            q1.push(x);
+            while(q2.empty()==false)
+            {
+                q1.push(q2.front());
+                q2.pop();
+            }
+        }
+    }
+    
+    int pop() {
+        int g=q1.front();
+        q1.pop();
+        return g;
+        
+    }
+    
+    int top() {
+        int g=q1.front();
+        return g;
+    }
+    
+    bool empty() {
+        if(q1.empty()==true)
+            return true;
+        else
+            return false;
+        
+    }
+};
