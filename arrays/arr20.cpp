946a2f985a8ea16cbb248109a5f03a51c7c1d001
nishanth0819 <87228964+nishanth0819@users.noreply.github.com>
2022-04-30 14:28:44 +0530

    Count Number of Nice Subarrays

---
+++ arrays/arr20.cpp
@@ -0,0 +1,38 @@
+// Given an array of integers nums and an integer k. A continuous subarray is called nice if there are k 
+// odd numbers on it.
+// Return the number of nice sub-arrays.
+
+// Example 1:
+// Input: nums = [1,1,2,1,1], k = 3
+// Output: 2
+// Explanation: The only sub-arrays with 3 odd numbers are [1,1,2,1] and [1,2,1,1].
+// Example 2:
+// Input: nums = [2,4,6], k = 1
+// Output: 0
+// Explanation: There is no odd numbers in the array.
+// Example 3:
+// Input: nums = [2,2,2,1,2,2,1,2,2,2], k = 2
+// Output: 16 
+
+// Constraints:
+// 1 <= nums.length <= 50000
+// 1 <= nums[i] <= 10^5
+// 1 <= k <= nums.length
+
+
+//CODE:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int n=nums.size();
+        int sum=0;
+        map<int,int> mp;
+        int count=0;
+        mp.insert({0,1});
+        for(int i=0;i<n;i++)
+        {
+            sum=sum+(nums[i]%2!=0?1:0);
+            if(mp.find(sum-k)!=mp.end())
+                count=count+mp[sum-k];
+            mp[sum]++;
+        }
+        return count;
+    }
