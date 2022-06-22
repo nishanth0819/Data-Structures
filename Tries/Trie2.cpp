df5059b947600cbc0922dde52169583231400c9a
nishanth0819 <87228964+nishanth0819@users.noreply.github.com>
2022-06-22 23:01:20 +0530

    Search a given key values is present in a trie

---
+++ Tries/Trie2.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+struct trienode
+{
+    struct trienode* children[26];
+    bool isendofword;
+};
+struct trienode* getnode()
+{
+    struct trienode* pnode=new trienode;
+    pnode->isendofword=false;
+    for(int i=0;i<26;i++)
+        pnode->children[i]=NULL;
+    return pnode;
+}
+void insert(struct trienode* root,string key)
+{
+    struct trienode *temp=root;
+    int n=key.size();
+    for(int i=0;i<n;i++)
+    {
+        int index=key[i]-'a';
+        if(!temp->children[index])
+            temp->children[index]=getnode();
+        temp=temp->children[index];
+    }
+    temp->isendofword=true;
+}
+bool iscontains(struct trienode *root,string key)
+{
+    struct trienode* temp=root;
+    int n=key.size();
+    for(int i=0;i<n;i++)
+    {
+        int index=key[i]-'a';
+        if(!temp->children[index])
+            return false;
+        temp=temp->children[index];
+    }
+    return temp->isendofword;
+}
+void display(struct trienode*root,char str[],int level)
+{
+    if(root->isendofword==true)
+    {
+        str[level]='\0';
+        cout<<str<<endl;
+    }
+    for(int i=0;i<26;i++)
+    {
+        if(root->children[i])
+        {
+            str[level]=i+'a';
+            display(root->children[i],str,level+1);
+        }
+    }
+}
+int main()
+{
+    struct trienode *root=getnode();
+    insert(root,"govardhan");
+    insert(root,"nishanth");
+    insert(root,"govardhansai");
+    char str[20];
+    display(root,str,0);
+    cout<<iscontains(root,"abc")<<endl;
+    cout<<iscontains(root,"govardhansai")<<endl;
+    return 0;
+}
